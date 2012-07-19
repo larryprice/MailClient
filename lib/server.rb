6844e5cdd37e208fb75f1a326d78014950396b7d
Larry Price <larry.price.dev@gmail.com>
2012-07-18 22:00:23 -0400

    Server class property tests - missing Type (IMAP,POP) and Direction (Incoming, Outgoing) -> does that matter?

--- lib/server.rb
+++ lib/server.rb
@@ -1,7 +1,74 @@
 #
 
 class Server
+  @@AuthenticationMethods = [:Normal_Password, :Encrypted_Password,
+    :Kerberos_AND_GSSAPI, :NTLM, :TLS_Certificate]
+  @@ConnectionSecurityTypes = [:None, :STARTTLS, :SSL_AND_TLS]
+  
   def initialize
     
+  end
+
+  # Name used to identify this server
+  def get_name
+    @name
+  end
+  def set_name(name)
+    @name = name
+  end
+
+  # Description for this server
+  def get_description
+    @description
+  end
+  def set_description(description)
+    @description = description
+  end
+
+  # Port number for this server
+  def get_port_number
+    @port_number
+  end
+  def set_port_number(port_number)
+    @port_number = port_number
+  end
+
+  # Username used to log in to this server
+  def get_username
+    @username
+  end
+  def set_username(username)
+    @username = username
+  end
+
+  # Authentication method to use to access this server
+  def get_authentication_method
+    humanize_enum @authentication_method
+  end
+  def set_authentication_method(method)
+    if @@AuthenticationMethods.include? method
+      @authentication_method = method
+    end
+  end
+
+  # Connection Security Type to use while accessing the server
+  def get_connection_security_type
+    humanize_enum @connection_security_type
+  end
+  def set_connection_security_type(connection_security_type)
+    if @@ConnectionSecurityTypes.include? connection_security_type
+      @connection_security_type = connection_security_type
+    end
+  end
+
+  def humanize_enum(enum)
+    if enum != nil
+      enum_string = enum.to_s
+      if enum_string.match(/AND/)
+        enum_string.gsub(/_AND_/, '/').gsub(/_/, ' ')
+      else
+        enum_string.gsub(/_/, ' ')
+      end
+    end
   end
 end
--- test/server_tests.rb
+++ test/server_tests.rb
@@ -19,4 +19,193 @@ class TC_MailCient < Test::Unit::TestCase
     assert_nil result, result
   end
 
+  def test_server_can_get_name
+    begin
+      @server.get_name
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_can_set_name
+    begin
+      @server.set_name(:name)
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_set_name_sets_name
+    @server.set_name(:name)
+    assert_equal :name, @server.get_name
+  end
+
+  def test_server_can_get_description
+    begin
+      @server.get_description
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_can_set_description
+    begin
+      @server.set_description(:description)
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_set_description_sets_description
+    @server.set_description(:description)
+    assert_equal :description, @server.get_description
+  end
+
+  def test_server_can_get_port_number
+    begin
+      @server.get_port_number
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_can_set_port_number
+    begin
+      @server.set_port_number(:port)
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_set_port_number_sets_port_number
+    @server.set_port_number(:port_number)
+    assert_equal :port_number, @server.get_port_number
+  end
+
+  def test_server_can_get_username
+    begin
+      @server.get_username
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_can_set_username
+    begin
+      @server.set_username(:username)
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_set_username_sets_username
+    @server.set_username(:username)
+    assert_equal :username, @server.get_username
+  end
+
+  def test_server_can_get_authentication_method
+    begin
+      @server.get_authentication_method
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_can_set_authentication_method
+    begin
+      @server.set_authentication_method(:method)
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_set_authentication_method_sets_authentication_method
+    @server.set_authentication_method(:Normal_Password)
+    assert_equal @server.humanize_enum(:Normal_Password),
+      @server.get_authentication_method
+  end
+
+  def test_server_set_authentication_only_accepts_valid_authentication_methods
+    assert_not_equal :method,
+      @server.set_authentication_method(:method)
+  end
+
+  def test_server_set_authentication_does_not_set_invalid_authentication_method
+    @server.set_authentication_method(:method)
+    assert_not_equal :method, @server.get_authentication_method
+  end
+
+  def test_server_get_authentication_method_returns_humanized_string
+    @server.set_authentication_method(:Kerberos_AND_GSSAPI)
+    assert_equal @server.humanize_enum(:Kerberos_AND_GSSAPI),
+      @server.get_authentication_method
+  end
+
+  def test_server_can_get_connection_security_type
+    begin
+      @server.get_connection_security_type
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_can_set_connection_security_type
+    begin
+      @server.set_connection_security_type(:connection)
+    rescue Exception => e
+      result = e.message
+    end
+
+    assert_nil result, result
+  end
+
+  def test_server_set_connection_security_type_sets_connection_security_type
+    @server.set_connection_security_type(:None)
+    assert_equal @server.humanize_enum(:None),
+      @server.get_connection_security_type
+  end
+
+  def test_server_set_security_does_not_set_invalid_security_type
+    assert_not_equal :connection,
+      @server.set_connection_security_type(:connection)
+  end
+
+  def test_server_get_security_type_returns_humanized_string
+    @server.set_connection_security_type(:SSL_AND_TLS)
+    assert_equal @server.humanize_enum(:SSL_AND_TLS),
+      @server.get_connection_security_type
+  end
+
+  def test_server_humanize_string_replaces_underscores_with_spaces
+    assert_equal "Lorem ipsum dolor sit amet",
+      @server.humanize_enum(:Lorem_ipsum_dolor_sit_amet)
+  end
+
+  def test_server_humanize_string_replaces_and_keyword_with_back_slash
+    assert_equal "Me/You/The Bottle",
+      @server.humanize_enum(:Me_AND_You_AND_The_Bottle)
+  end
+
 end
